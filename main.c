7d2cf9c112474ff2652ca557c8d148c0b5a34861
scheibs@mail.gvsu.edu <scheibs@mail.gvsu.edu>
2018-07-31 12:06:39 -0400

    Working Project

--- main.c
+++ main.c
@@ -4,15 +4,11 @@
 #include <dirent.h>
 #include <openssl/sha.h>
 
-#define NODE_DATA_SIZE 1000
-#define NUM_NODES 30
+#define NODE_DATA_SIZE 500
+#define NUM_NODES 500
 #define DIR_PATH "./files"
 
-struct node
-{
-    char data[NODE_DATA_SIZE];
-    struct node *parent;
-};
+char *getSHA(unsigned char *data);
 
 int main()
 {
@@ -20,13 +16,12 @@ int main()
     FILE *fp;
     int i = 0;
     struct dirent *dir;
-    struct node nodes[NUM_NODES];
+    char data[NUM_NODES][NODE_DATA_SIZE];
     char temp[NODE_DATA_SIZE];
     char filename[100];
     unsigned char allData[NODE_DATA_SIZE*NUM_NODES];
 
-    unsigned char hash[SHA_DIGEST_LENGTH];
-    char buf[SHA_DIGEST_LENGTH*2];
+    char currSHA[SHA_DIGEST_LENGTH*2];
 
     //get all text from directory
     d = opendir(DIR_PATH);
@@ -44,7 +39,7 @@ int main()
             else
             {
                 fgets(temp, NODE_DATA_SIZE, fp);
-                strcpy(nodes[i].data, temp);
+                strcpy(data[i], temp);
                 fclose(fp);
             }
 
@@ -55,30 +50,46 @@ int main()
         closedir(d);
     }
 
-    for(i=0; i<NUM_NODES; i++)
-    {
-        strcpy(temp, nodes[i].data);
-        strcat(allData, temp);
-        //printf("Stored: %s\n", nodes[i].data);
+    for(i=1; i<NUM_NODES; i++){
+        if(i==1)
+            strcpy(currSHA, getSHA(strcat(data[i], data[i-1])));
+
+        strcpy(currSHA, getSHA(strcat(currSHA, data[i])));
     }
+
+    printf("%s\n", currSHA);
 
     /*
+    for(i=0; i<NUM_NODES; i++)
+    {
+        strcpy(temp, data[i]);
+        strcat(allData, temp);
+        //printf("Stored: %s\n", data[i]);
+    }
+    printf("%s\n", getSHA(allData));
 
-    SHA GEN WORKING
+    //SHA GEN WORKING
 
     */
+    return 0;
+}
+
+char *getSHA(unsigned char *data)
+{
+    int i = 0;
+    unsigned char temp[SHA_DIGEST_LENGTH];
+    char *buf = malloc(SHA_DIGEST_LENGTH*2);
 
     memset(buf, 0x0, SHA_DIGEST_LENGTH*2);
-    memset(hash, 0x0, SHA_DIGEST_LENGTH);
+    memset(temp, 0x0, SHA_DIGEST_LENGTH);
 
-    SHA1(allData, strlen(allData), hash);
+    SHA1((unsigned char *)data, strlen(data), temp);
 
-    for (i=0; i < SHA_DIGEST_LENGTH; i++)
-    {
-        sprintf((char*)&(buf[i*2]), "%02x", hash[i]);
+    for (i=0; i < SHA_DIGEST_LENGTH; i++) {
+        sprintf((char*)&(buf[i*2]), "%02x", temp[i]);
     }
 
-    printf("SHA1: %s\n", buf);
+    return buf;
 
     return 0;
 }
